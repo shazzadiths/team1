ed6002bf63f5a5154d9933ea8f0ffa8e9a11c29a
Shazzad Hossain <m.shazzad@gmail.com>
2023-05-09 18:49:13 +0200

    test mapp

---
+++ src/test/java/com/example/produktapi/model/ProductUnitTest.java
@@ -0,0 +1,29 @@
+package com.example.produktapi.model;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions;
+
+
+import java.util.List;
+
+
+@SpringBootTest
+public class ProductUnitTest {
+
+
+    Product product = new Product();
+
+    //public Product(String title, Double price, String category, String description, String image)
+
+
+    @Test
+    void CheckProductTitle() {
+        product.setTitle("Powerboots Original Stövlar");
+      String newtitle = product.getTitle();
+      Assertions.assertEquals( newtitle,"Powerboots Original Stövlar");
+// Test commit
+    }
+   
+
+
+}
