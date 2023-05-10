6c2ea426537a3bec4096c3aca7df90efd5a2646e
chvijayalakshmi <challavijayalakshmi8@gmail.com>
2023-05-10 21:48:12 +0200

    product ID test

--- src/test/java/com/example/produktapi/model/ProductUnitTest.java
+++ src/test/java/com/example/produktapi/model/ProductUnitTest.java
@@ -14,40 +14,10 @@ public class ProductUnitTest {
     Product product = new Product();
 
     @Test
-    void CheckProductTitle() {
-    product.setTitle("Powerboots Original Stövlar");
-    String newtitle = product.getTitle();
-    Assertions.assertEquals( newtitle,"Powerboots Original Stövlar");
-// Test commit
+    public void testGetID(){
+        product.setId(101);
+        Assertions.assertEquals(Integer.valueOf(101),product.getId());
     }
-
-    @Test
-    //EmmaDahl
-    void CheckGetCategory(){
-    product.setCategory("Jewelery");
-    String getCategory = product.getCategory();
-    Assertions.assertEquals(getCategory, "Jewelery");
-    }
-
-    @Test
-//Somayeh
-    public void testGetPrise(){
-
-        product.setPrice(200.00);
-        double price = product.getPrice();
-        Assertions.assertEquals(200.00, price);
-    }
-    @Test
-    public void GetImageTest(){
-        product.setImage("https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg");
-        Assertions.assertEquals("https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",product.getImage());
-    }
-    @Test
-    public void GetDescriptionTest(){
-        product.setDescription("sldjdsfvndlkvnfdlvjfjl");
-        Assertions.assertEquals("sldjdsfvndlkvnfdlvjfjl",product.getDescription());
-    }
-
 
 }
 
