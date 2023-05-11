5058dcabc24f57700df82012dc52d8291d66cda3
shazzadiths <133035243+shazzadiths@users.noreply.github.com>
2023-05-11 16:43:11 +0200

    Revert "Vijaya"

--- src/test/java/com/example/produktapi/model/ProductUnitTest.java
+++ src/test/java/com/example/produktapi/model/ProductUnitTest.java
@@ -11,12 +11,32 @@ import java.util.List;
 @SpringBootTest
 public class ProductUnitTest {
 
+
     Product product = new Product();
 
     @Test
-    public void testGetID(){
-        product.setId(101);
-        Assertions.assertEquals(Integer.valueOf(101),product.getId());
+    void CheckProductTitle() {
+    product.setTitle("Powerboots Original Stövlar");
+    String newtitle = product.getTitle();
+    Assertions.assertEquals( newtitle,"Powerboots Original Stövlar");
+// Test commit
+    }
+
+    @Test
+    //EmmaDahl
+    void CheckGetCategory(){
+    product.setCategory("Jewelery");
+    String getCategory = product.getCategory();
+    Assertions.assertEquals(getCategory, "Jewelery");
+    }
+
+    @Test
+//Somayeh
+    public void testGetPrise(){
+
+        product.setPrice(200.00);
+        double price = product.getPrice();
+        Assertions.assertEquals(200.00, price);
     }
 
 }
