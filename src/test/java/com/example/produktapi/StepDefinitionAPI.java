566ba6f1ed5b60bc0e37346557bf81430247ff9c
emmadahl <emmadahl80@gmail.com>
2023-05-17 15:19:32 +0200

    Added name comment
    Signed-off-by: emmadahl <emmadahl80@gmail.com>

--- src/test/java/com/example/produktapi/StepDefinitionAPI.java
+++ src/test/java/com/example/produktapi/StepDefinitionAPI.java
@@ -13,12 +13,14 @@ public class StepDefinitionAPI {
     private static RequestSpecification request;
     private static Response response;
 
+    //Emma Dahl
     @When("User make GET-request to {string}")
     public void user_make_get_request_to(String endpoint) {
         baseURI = endpoint;
         request = given();
         response = request.request(Method.GET,"");
     }
+    //Emma Dahl
     @Then("HTTP respons status code should be {int}")
     public void http_respons_status_code_should_be(Integer expectedStatusCode) {
         int actualStatusCode = response.statusCode();
